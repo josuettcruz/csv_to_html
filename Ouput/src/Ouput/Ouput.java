6110daa56ce36981b9a3824e73ba9ac045b071da
Josué Trindade Torres Cruz <josuettcruz.jttc@gmail.com>
2025-06-05 11:40:34 -0300

    11:40 05/06/2025 - Inserindo novo projeto Java!

---
+++ Ouput/src/Ouput/Ouput.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ouput;
+
+import form.Converter;
+
+/**
+ *
+ * @author josue
+ */
+public class Ouput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Converter().Enter();
+        
+    }
+    
+}
